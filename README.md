ff150b2c07ea107e219548f349756e22e21be3bf
Muhammad Bilal <muhammadbx4@gmail.com>
2023-04-26 19:52:50 +0500

    commit

--- README.md
+++ README.md
@@ -6,3 +6,22 @@
 - mongoose
 - express
 - nodemon
+
+
+
+progress: udemy course  
+
+video 409 - creating products
+    - make a /product/new route to get new product from form.   [done]
+    - make a post handler /products route   [done]
+    - app.use(express.urlencoded({extended: true})) for parsing post body   [done]
+    - new product(req.body)     [done]
+    - no validations yet    [done]
+    - finished   [done]
+
+video 410 - updating products
+    - 
+
+video 411 - creating products
+video 412 - deleting products
+video 413 - filtering by category
--- index.js
+++ index.js
@@ -20,17 +20,57 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmdb')
 app.set('views', path.join(__dirname, 'views'))    
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({extended: true}))
+
 //RESTful routes
 app.get('/', (req, res)=>{
     res.send('<h1>Welcome</h1>')
 })
-
+// show all products
 app.get('/products', async (req, res)=>{
     const products = await Product.find({})
     res.render('products/index', {products})
 })
+// create new products
+app.get('/products/new', (req, res)=>{
+    res.render('products/new')
+})
+// create new product post req handler
+app.post('/products', async (req, res)=>{
+    // console.log(req.body)
+    const newProduct = new Product(req.body)
+    await newProduct.save()
+    res.redirect('/products')
+}) 
+
+// show a specific product by id
+app.get('/products/:id', async (req, res)=>{
+    const {id} = req.params
+    const product = await Product.findById(id)
+    res.render('products/details', {product})
+})
+
 
 
 app.listen(PORT, ()=>{
     console.log('SERVER LISTENING ON PORT: ', PORT)    
 })
+
+
+
+// samples
+// {
+//     _id: ObjectId("6448b895e858c172aa28f93d"),  
+//     name: 'Chocolate Whole Milk',
+//     price: 2.69,
+//     category: 'dairy',
+//     __v: 0
+//   },
+//   {
+    // _id: ObjectId("6448b895e858c172aa28f93e"),  
+//     name: 'Organic Celery',
+//     price: 1.5,
+//     category: 'vegetable',
+//     __v: 0
+//   }
+
